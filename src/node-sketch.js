689acc770dd12102b417d5d8f2508c0223419083
Parker Henderson <parker@parkerhendo.com>
2019-05-10 17:44:52 -0700

    idk what changed

---
+++ src/node-sketch.js
@@ -0,0 +1,11 @@
+const ns = require('node-sketch');
+const path = require('path');
+
+const getColors = require('./color/getColors');
+const returnDocumentOpacities = require('./color/getOpacities');
+
+(async function() {
+//   const sketch = await ns.read(path.join(__dirname, '../', 'design.sketch'));
+  await getColors(sketch)
+//   await returnDocumentOpacities(sketch, 0);
+})();
